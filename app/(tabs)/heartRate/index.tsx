098f519a110b65e64d5575ebfec09fe43e85cf79
atk1432 <hiepnguyenvietatk@gmail.com>
2025-01-23 00:23:09 +0700

    update tflite test model

--- app/(tabs)/heartRate/index.tsx
+++ app/(tabs)/heartRate/index.tsx
@@ -2,57 +2,68 @@ import ButtonNormal from "@/components/buttons/ButtonNormal";
 import { _Layout, _Text } from "@/components/ultis";
 import { useEffect, useState, useRef } from "react";
 import { StyleSheet, View } from "react-native";
-import { Camera, useCameraPermission, useCameraDevice, useCameraFormat, CameraDevice, useFrameProcessor } from "react-native-vision-camera"
+import { Camera, useCameraPermission, useCameraDevice, useCameraFormat, useFrameProcessor } from "react-native-vision-camera"
 import Torch from "react-native-torch"
-import { useTensorflowModel } from "react-native-fast-tflite";
+import { useTensorflowModel, loadTensorflowModel } from "react-native-fast-tflite";
 import {useResizePlugin  } from "vision-camera-resize-plugin"
+import { useSharedValue } from "react-native-worklets-core";
+import { useSharedValue as _useSharedValue } from "react-native-reanimated"
+import { Asset, useAssets } from "expo-asset";
+
 
 const cameraSize = 300
 
 export default function HeartRateScreen() {
   const [ cameraActive, setCameraActive ] = useState(false)
+  const [ assets, error ] = useAssets([require('@/assets/models/test_model.tflite')])
   const device : any = useCameraDevice('back')
   const lowResolutionFormat = useCameraFormat(device, [
     { videoResolution: { width: 640, height: 480 } },
   ])
   const { hasPermission, requestPermission } = useCameraPermission()
+  const rgbMeans = useSharedValue(0)
+  const shareValue = _useSharedValue(rgbMeans)
 
   // Plugins
   const { resize } = useResizePlugin()
 
   // Models
-  const objectDetection = useTensorflowModel(require('@/assets/models/1.tflite'))
+  // console.log(assets)
+  const plugin = useTensorflowModel(require('@/assets/models/test_model.tflite'))
+  const model = plugin.state === "loaded" ? plugin.model : undefined
+  // console.log(model)
 
   const frameProcessor = useFrameProcessor((frame) => {
     'worklet'
+    if (model == null) return
+
     const resized = resize(frame, {
       scale: {
-        width: 320,
-        height: 320,
+        width: 128,
+        height: 128,
       },
       pixelFormat: 'rgb',
-      dataType: 'float32',
+      dataType: "uint8",
     })
 
-    // const outputs = objectDetection.model?.runSync([frame])
-  }, [])
+    // if (!model)
+    const outputs: any = model?.runSync([resized])
+    // console.log(outputs)
+    rgbMeans.value = outputs[0][0]
+
+  }, [model])
 
   useEffect(() => {
     async function checkPermission() {
-      const cameraAllowed = await Torch.requestCameraPermission(
-          'Camera Permissions', // dialog title
-          'We require camera permissions to use the torch on the back of your phone.' // dialog body
-      );
-  
-      if (cameraAllowed) {
-        console.log('Flashlight allowed')
-      } else {
-        console.log('Flashlight not allowed')
-      }
+      // Load model
+      // var model = await loadTensorflowModel(require('@/assets/models/test_model.tflite'))
+      // setModel(model)
     }
 
     checkPermission()
   }, [])
+
+  // console.log(rgbMeans.get())
 
   return (
     <_Layout size="full" style={ styles.layout }>
@@ -69,6 +80,7 @@ export default function HeartRateScreen() {
           /> 
         : "" }
       </View>
+      <_Text>Red: { rgbMeans.value }</_Text>
       <ButtonNormal 
         style={ styles.button }
         onPress={ () => {
